0898c45bc014ce8aac3c7014f085f8e65138f0c4
Pulipuli Chen <blog@pulipuli.info>
2021-10-22 04:13:53 +0800

    1022 Mobile01

--- api/lib/HtmlLoader/XMLLoader.js
+++ api/lib/HtmlLoader/XMLLoader.js
@@ -1,19 +1,20 @@
 const HtmlLoader = require('./HtmlLoader.js')
 const PuppeterHTMLLoader = require('./PuppeterHTMLLoader.js')
 
+const cheerio = require('cheerio')
+
 const XMLLoader = async function (url, cacheMS) {
   let feedXML = await HtmlLoader(url, cacheMS)
+  let $ = cheerio.load(feedXML)
   //console.log('~~~', feedXML, '~~~')
-  if (feedXML.indexOf('<html') > -1) {
+  if ($('html').length > 0) {
     //console.log('go')
     feedXML = await PuppeterHTMLLoader(url, 1)
     //console.log('***', feedXML, '***')
-    if (feedXML.indexOf('<rss') > -1) {
-      feedXML = feedXML.slice(feedXML.indexOf('<rss'))
-    }
+    $ = cheerio.load(feedXML)
     
-    if (feedXML.indexOf('</rss>') > -1) {
-      feedXML = feedXML.slice(0, feedXML.indexOf('</rss>') + 6)
+    if ($('rss:first').length === 1) {
+      feedXML = $('rss:first').prop('outerHTML')
     }
     
     //console.log(feedXML)
