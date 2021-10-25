50fdae19136f00b56b967f97d9238d19fbbbbb51
Pulipuli Chen <blog@pulipuli.info>
2021-10-26 00:30:06 +0800

    1025 一個一個來修正ACG Feed的問題吧

--- api/lib/xmlTransformers/FeedChannelTitle.js
+++ api/lib/xmlTransformers/FeedChannelTitle.js
@@ -1,12 +1,22 @@
+let appendText = '++'
+
+const removeAppendText = function (title) {
+  let text = title.text().trim()
+  if (text.endsWith(appendText)) {
+    text = text.slice(0, -2)
+  }
+  return text
+} 
+
 const FeedChannelTitle = function ($) {
   let title = $('channel > title:first')
   if (title.length === 1) {
-    return title.text().trim()
+    return removeAppendText(title)
   }
   
   title = $('feed > title:first')
   if (title.length === 1) {
-    return title.text().trim()
+    return removeAppendText(title)
   }
   
   throw Error('Title is not found')
