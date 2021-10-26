83ecb5a40574d334871e18d0fbf3bece29a1f698
Pulipuli Chen <blog@pulipuli.info>
2021-10-27 01:42:22 +0800

    1027 零度解說

--- modules/xmlTransformers/xUB.js
+++ modules/xmlTransformers/xUB.js
@@ -1,6 +1,7 @@
 const FeedItemEach = require('./../../api/lib/xmlTransformers/FeedItemEach.js')
 const FeedItemGetLink = require('./../../api/lib/xmlTransformers/FeedItemGetLink.js')
 const FeedItemGetContent = require('./../../api/lib/xmlTransformers/FeedItemGetContent.js')
+const FeedItemGetTitle = require('./../../api/lib/xmlTransformers/FeedItemGetTitle.js')
 const FeedItemSetContent = require('./../../api/lib/xmlTransformers/FeedItemSetContent.js')
 
 const ModuleManager = require('./../../api/lib/ModuleManager/ModuleManager.js')
@@ -50,9 +51,31 @@ const xUB = async function ($, moduleCodesString) {
     let captions = await xUBGetCaptions(videoID)
     
     if (!captions || captions.length === 0) {
-      return item.remove()
+      //return item.remove()
+      content = buildThumbnails(videoID) + '<br /><hr /><br />' + formattedContent
+    }
+    else {
+      content = setupContentWithCaption(formattedContent, sections, captions, videoID)
+      let title = FeedItemGetTitle(item)
+      title = '[C]' + title
+      item.find('title:first').text(title)
     }
     
+    FeedItemSetContent(item, content)
+    
+//    let title = item.find('title').text()
+//    let titleNew = await ModuleManager(title, moduleCodesString, 't')
+//    if (title !== titleNew) {
+//      item.find('title').text(titleNew)
+//    }
+//    console.log(moduleCodesString, titleNew)
+  })
+  
+  return $
+}
+
+const setupContentWithCaption = function (formattedContent, sections, captions, videoID) {
+  
     if (sections.length < 2) {
       let result = xUBBuildSectionFromCaptions(captions)
       sections = result.sections
@@ -69,16 +92,7 @@ const xUB = async function ($, moduleCodesString) {
     
     let sectionsTOC = xUBBuildSectionTOC(sections, videoID)
     
-    let thumbnails = `<p>
-  <img src="http://i3.ytimg.com/vi/${videoID}/maxresdefault.jpg" style="max-width:100%;height: auto;" />
-</p>
-<p>
-  <img src="https://img.youtube.com/vi/${videoID}/1.jpg" style="max-width:100%;height: auto;" />
-  <img src="https://img.youtube.com/vi/${videoID}/2.jpg" style="max-width:100%;height: auto;" />
-  <img src="https://img.youtube.com/vi/${videoID}/3.jpg" style="max-width:100%;height: auto;" />
-</p>`
-    
-    content = [thumbnails]
+    content = [buildThumbnails(videoID)]
     if (sectionsTOC) {
       content.push(sectionsTOC)
       content.push('<hr />')
@@ -92,10 +106,21 @@ const xUB = async function ($, moduleCodesString) {
     }
     content = content.join('\n')
     
-    FeedItemSetContent(item, content)
-  })
+    return content
+}
+
+const buildThumbnails = function (videoID) {
   
-  return $
+    let thumbnails = `<p>
+  <img src="http://i3.ytimg.com/vi/${videoID}/maxresdefault.jpg" style="max-width:100%;height: auto;" />
+</p>
+<p>
+  <img src="https://img.youtube.com/vi/${videoID}/1.jpg" style="max-width:100%;height: auto;" />
+  <img src="https://img.youtube.com/vi/${videoID}/2.jpg" style="max-width:100%;height: auto;" />
+  <img src="https://img.youtube.com/vi/${videoID}/3.jpg" style="max-width:100%;height: auto;" />
+</p>`
+    
+  return thumbnails
 }
 
 module.exports = xUB
