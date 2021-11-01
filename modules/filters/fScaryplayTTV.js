97e7c06234a88fc4df1ea89ba76e69ba91847ef5
Pulipuli Chen <pulipuli.chen@gmail.com>
2021-11-01 22:30:14 +0800

    1101 Linux APP

--- modules/filters/fScaryplayTTV.js
+++ modules/filters/fScaryplayTTV.js
@@ -1,7 +1,10 @@
 const fScaryplayTTV = function ({item, channel}) {
   let title = item.find('title').text().trim()
   
-  return (!title.endsWith('希治閣Podcast'))
+  return (!title.endsWith('希治閣Podcast')
+          && !title.startsWith('突然直播')
+          && !title.startsWith('【特別推介】')
+          && (title.indexOf('八卦新聞') > -1 || title.indexOf('情報科') > -1 ))
 }
 
 module.exports = fScaryplayTTV
