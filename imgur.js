2f7f09c92d203bd4153cbd7200439383d7fdbc22
Pulipuli Chen <pulipuli.chen@gmail.com>
2021-12-28 03:51:10 +0800

    1228 imgur

--- imgur.js
+++ imgur.js
@@ -14,7 +14,7 @@ async function urlToImgur(url) {
   let tmpFilePath = '/tmp/' + (new Date()).getTime() + '.jpg'
   return new Promise(resolve => {
 	download(url, tmpFilePath, function(){
-		imgur.upload(tmpFilePath),function(err, res){
+		imgur.upload(tmpFilePath,function(err, res){
 		  //console.log(res.data.link); //log the imgur url
 		  resolve(res.data.link)
 		});
@@ -29,6 +29,6 @@ var download = function(uri, filename, callback){
   
 	  request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
 	});
-  };
+};
   
 console.log(await urlToImgur('https://scontent-dus1-1.xx.fbcdn.net/v/t39.30808-6/s720x720/260203522_451044079975678_3458594004275259140_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=110474&_nc_ohc=hzqkU6UuUJEAX_D8VAI&_nc_ht=scontent-dus1-1.xx&edm=AJdBtusEAAAA&oh=00_AT_2TRJgFtmF6mjA6fhcS8wT3j0DOAMU1bhDaPXj83n1Xw&oe=61CF56CC'))
