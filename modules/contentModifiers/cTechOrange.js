944e4bc124b146dbf329c020fadb751897272037
Pulipuli Chen <pulipuli.chen@gmail.com>
2022-01-15 15:06:44 +0800

    0115 to

--- modules/contentModifiers/cTechOrange.js
+++ modules/contentModifiers/cTechOrange.js
@@ -5,7 +5,7 @@ const cTechbang = function (content) {
   const $ = cheerio.load(content)
   
   let thumb = $('.entry-thumb.single-entry-thumb:first')
-  let body = $('.single-body--content:first')
+  let body = $('.single-body--content:first > .single-content__wrap:first')
 
   if (thumb.length > 0 && body.length > 0) {
     body.prepend(thumb)
