37c2ad38d51127da2180f92753f3ef7c6bbad601
Pulipuli Chen <pulipuli.chen@gmail.com>
2022-01-17 01:43:59 +0800

    RSS

--- api/lib/HtmlLoader/XMLLoader.js
+++ api/lib/HtmlLoader/XMLLoader.js
@@ -26,7 +26,7 @@ const XMLLoader = async function (url, cacheMS) {
     //feedXML = feedXML.normalize('NFD').replace(/([\u0300-\u036f]|[\u001d])/g, ' ');
     //var bytelike= unescape(encodeURIComponent(feedXML));
     //feedXML= decodeURIComponent(escape(bytelike));
-    feedXML = feedXML.replace(/\u0x1d/g, '')
+    feedXML = feedXML.replace(/[\x00-\x1F\x7F]/g , '')
 
     let $ = cheerio.load(feedXML)
     //console.log('~~~', feedXML, '~~~')
