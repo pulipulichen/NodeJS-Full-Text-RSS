1520e0a0ea076535013ef67bd178a9fb8777ad50
Pulipuli Chen <pulipuli.chen@gmail.com>
2022-03-22 15:15:31 +0800

    0322 Pulipuli Blog

--- modules/contentModifiers/cPulipuliBlog.js
+++ modules/contentModifiers/cPulipuliBlog.js
@@ -2,8 +2,20 @@ const cheerio = require('cheerio')
 
 const cPulipuliBlog = function (content) {
   let container = cheerio.load(content);
-  let text = container('p').eq(0).text()
+  let text = []
+  let pList = container('p')
+  for (let i = 0; i < pList.length; i++) {
+    let p = pList.eq(0)
+    let t = p.text().trim()
+    
+    text.push(t)
+    
+    if (text.join('').length > 250) {
+      break
+    }
+  }
   
+  text = text.join('\n\n')
   console.log(text)
   
   return text
