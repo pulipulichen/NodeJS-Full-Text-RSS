ee6731f435dce14403cadf4ad0441e12bf2d9da7
Pulipuli Chen <pulipuli.chen@gmail.com>
2022-03-22 14:57:15 +0800

    0322 Pulipuli Blog

--- modules/xmlTransformers/xPulipuliBlog.js
+++ modules/xmlTransformers/xPulipuliBlog.js
@@ -1,5 +1,16 @@
 
 const xSkip = async function ($, moduleCodesString) {
+  
+  let thumbnails = $(`media\:thumbnail[url$="=s72-c"][height="72"][width="72"]`)
+  for (let i = 0; i < thumbnails.length; i++) {
+    let thumbnail = thumbnails.eq(i)
+    
+    thumbnail.removeAttr('height')
+    thumbnail.removeAttr('width')
+    let url = thumbnail.attr('url')
+    url = url.slice(0, -6)
+    thumbnail.attr('url', url)
+  }
 
   return $
 }
