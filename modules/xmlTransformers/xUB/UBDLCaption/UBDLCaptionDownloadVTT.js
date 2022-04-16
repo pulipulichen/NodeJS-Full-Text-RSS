b70e60667895e43395ef56e415a7159d0d5677ff
Pulipuli Chen <pulipuli.chen@gmail.com>
2022-04-16 22:20:41 +0800

    Sat Apr 16 10:20:41 PM CST 2022

--- modules/xmlTransformers/xUB/UBDLCaption/UBDLCaptionDownloadVTT.js
+++ modules/xmlTransformers/xUB/UBDLCaption/UBDLCaptionDownloadVTT.js
@@ -3,33 +3,37 @@ const UBDLCaptionPreferLang = require('./UBDLCaptionPreferLang.js')
 const { exec } = require("child_process")
 const fs = require("fs")
 
+const NodeCacheSQLite = require('./../../../../api/lib/cache/node-cache-sqlite.js')
+
 module.exports = async function (videoID) {
   let lang = await UBDLCaptionPreferLang(videoID)
   if (lang === false) {
     return ''
   }
 
-  let oPath = '/tmp/ubdl/' + videoID
-  let targetPath = '/tmp/ubdl/' + videoID + '.' + lang + '.vtt'
+  return await NodeCacheSQLite.get('UBDLCaptionDownloadVTT', videoID + ',' + lang), () => {
+    let oPath = '/tmp/ubdl/' + videoID
+    let targetPath = '/tmp/ubdl/' + videoID + '.' + lang + '.vtt'
 
-  let cmd = 'yo' + 'utu' + 'be-dl --write-sub --sub-lang ' + lang + ' --skip-download -o "' + oPath + '" https://www.yo' + 'utu' + 'be.com/watch?v=' + videoID
-  console.log(cmd)
-  return new Promise((resolve, reject) => {
-    exec(cmd, (err, stdout, stderr) => {
-      if (fs.existsSync(targetPath)) {
-        fs.readFile(targetPath, 'utf8', function(err, data) {
-          fs.unlinkSync(targetPath)
-          if (err) {
-            //throw err;
-            return reject(err)
-          }
-          //console.log(data);
-          return resolve(data.trim())
-        })
-      }
-      else {
-        reject('Download failed: ' + videoID)
-      }
+    let cmd = 'yo' + 'utu' + 'be-dl --write-sub --sub-lang ' + lang + ' --skip-download -o "' + oPath + '" https://www.yo' + 'utu' + 'be.com/watch?v=' + videoID
+    console.log(cmd)
+    return new Promise((resolve, reject) => {
+      exec(cmd, (err, stdout, stderr) => {
+        if (fs.existsSync(targetPath)) {
+          fs.readFile(targetPath, 'utf8', function(err, data) {
+            fs.unlinkSync(targetPath)
+            if (err) {
+              //throw err;
+              return reject(err)
+            }
+            //console.log(data);
+            return resolve(data.trim())
+          })
+        }
+        else {
+          reject('Download failed: ' + videoID)
+        }
+      })
     })
   })
 }
--- modules/xmlTransformers/xUB/UBDLCaption/UBDLCaptionListSubs.js
+++ modules/xmlTransformers/xUB/UBDLCaption/UBDLCaptionListSubs.js
@@ -1,31 +1,35 @@
 const { exec } = require("child_process")
+const NodeCacheSQLite = require('./../../../../api/lib/cache/node-cache-sqlite.js')
 
-module.exports = function (videoID) {
-  return new Promise((resolve, reject) => {
-    let cmd = 'yo' + 'utu' + 'be-dl --list-subs https://www.yo' + 'utu' + 'be.com/watch?v=' + videoID
+module.exports = async function (videoID) {
+  return await NodeCacheSQLite.get('UBDLCaptionListSubs', videoID, () => {
+    return new Promise((resolve, reject) => {
+      let cmd = 'yo' + 'utu' + 'be-dl --list-subs https://www.yo' + 'utu' + 'be.com/watch?v=' + videoID
 
-    exec(cmd, (err, stdout, stderr) => {
-      let lines = stdout.split('\n')
-      let langs = []
+      exec(cmd, (err, stdout, stderr) => {
+        let lines = stdout.split('\n')
+        let langs = []
 
-      let nextIsLang = false
-      for (let i = 0; i < lines.length; i++) {
-        let line = lines[i]
+        let nextIsLang = false
+        for (let i = 0; i < lines.length; i++) {
+          let line = lines[i]
 
-        if (line.startsWith('Language') && line.endsWith('formats')) {
-          nextIsLang = true
-          continue
+          if (line.startsWith('Language') && line.endsWith('formats')) {
+            nextIsLang = true
+            continue
+          }
+
+          if (nextIsLang) {
+            let parts = line.split(' ')
+            let lang = parts[0].trim()
+
+            langs.push(lang)
+          }
         }
 
-        if (nextIsLang) {
-          let parts = line.split(' ')
-          let lang = parts[0].trim()
-
-          langs.push(lang)
-        }
-      }
-
-      return resolve(langs)
+        return resolve(langs)
+      })
     })
   })
+    
 }
