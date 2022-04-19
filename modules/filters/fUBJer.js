ad62bf997f5cb0e07e41896f6c3ea803a16f30af
Pulipuli Chen <pulipuli.chen@gmail.com>
2022-04-19 17:26:14 +0800

    Tue Apr 19 05:26:14 PM CST 2022

--- modules/filters/fUBJer.js
+++ modules/filters/fUBJer.js
@@ -1,7 +1,7 @@
 const fUBJer = function ({item, channel}) {
   let title = item.find('title').text().trim()
   
-  return (title.indexOf('直播') > -1)
+  return (title.indexOf('直播') === -1)
 }
 
 module.exports = fUBJer
