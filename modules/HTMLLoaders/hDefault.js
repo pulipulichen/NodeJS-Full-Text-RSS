5314508d3f33661bdd8734de0fce0a1e67d79caf
Pulipuli Chen <pulipuli.chen@gmail.com>
2022-09-01 18:46:06 +0800

    Thu Sep  1 06:46:06 PM CST 2022

--- modules/HTMLLoaders/hDefault.js
+++ modules/HTMLLoaders/hDefault.js
@@ -2,9 +2,14 @@ const HtmlLoader = require('./../../api/lib/HtmlLoader/HtmlLoader.js')
 //const PuppeterHTMLLoader = require('./../../api/lib/HtmlLoader/PuppeterHTMLLoader.js')
 
 const hDefault = async function (url, modules) {
-  let html = await HtmlLoader(url)
-  
-  return html
+  try {
+    let html = await HtmlLoader(url)
+    
+    return html
+  }
+  catch (e) {
+    return false
+  }
 }
 
 module.exports = hDefault
