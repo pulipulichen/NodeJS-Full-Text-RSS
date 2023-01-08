3b28c3aab56080a5fbe50759ff78a21c5c70b608
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-01-09 03:45:12 +0800

    Mon Jan  9 03:45:12 AM CST 2023

--- api/feed-transformer/DetectFeedModule.js
+++ api/feed-transformer/DetectFeedModule.js
@@ -57,7 +57,10 @@ const DetectFeedModule = function ($, moduleCodesString) {
   
   if (channelLink.startsWith('https://blog.pulipuli.info/')) {
     addModules.push('xPulipuliBlog')
-    addModules.push('cPulipuliBlog')
+    if (moduleCodesString.indexOf('cPulipuliBlogPinterest') === -1) {
+      addModules.push('cPulipuliBlog')
+    }
+      
   }
   if (channelLink === 'https://buzzorange.com/techorange') {
     addModules.push('cTechOrange')
---
+++ api/lib/xmlTransformers/FeedItemEach.js
@@ -0,0 +1,55 @@
+const cheerio = require('cheerio')
+const sleep = require('./../async/sleep.js')
+
+const config = require('./../../../mount/config.js')
+
+const FeedItemEach = async function ($, handler) {
+  
+  let items = $('channel > item')
+  //console.log('items.length', items.length)
+  
+  if (items.length === 0) {
+    items = $('feed > entry')
+    //console.log(items.length)
+  }
+  if (items.length === 0) {
+    items = $('item')
+    //console.log(items.length)
+  }
+  if (items.length === 0) {
+    items = $('entry')
+    //console.log(items.length)
+  }
+  
+  // let titleElements = $('title')
+  // for (let i = 0; i < titleElements.length; i++) {
+  //   titleElements.eq(i).html(titleElements.eq(i).text())
+  // }
+  $('title').html('測試')
+
+  let len = items.length
+  if (len > config.FeedItemEach.limit) {
+    len = config.FeedItemEach.limit
+  }
+  
+  for (let i = 0; i < len; i++) {
+    let item = items.eq(i)
+    await handler(item, i)
+    //console.log(i)
+  }
+  for (let i = len; i < items.length; i++) {
+    let item = items.eq(i)
+    item.remove()
+    //console.log(i)
+  }
+  await sleep(50)
+  
+//  if (items.length === 0) {
+//    console.log($.html())
+//    console.error('Item is not found.')
+//  }
+  
+  return $
+}
+
+module.exports = FeedItemEach
