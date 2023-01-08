bda2c1892cd2302e7f8c79b436e88c4190d5d4bc
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-01-08 21:41:11 +0800

    Sun Jan  8 09:41:11 PM CST 2023

--- api/feed-crawler/route.js
+++ api/feed-crawler/route.js
@@ -152,9 +152,10 @@ const route = function (app) {
       }
 
       // cacheExpireTime = 1000
-      let result = await NodeCacheSQLite.get('feed-crawler', url, async () => {
-        return await FeedCrawler(url)
-      }, cacheExpireTime)
+      // let result = await NodeCacheSQLite.get('feed-crawler', url, async () => {
+      //   return await FeedCrawler(url)
+      // }, cacheExpireTime)
+      let result = await FeedCrawler(url)
 
       setHeader(res)
 
