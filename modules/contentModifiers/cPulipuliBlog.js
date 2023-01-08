72d624d19bd6f33144c2848aa4a3eabdf2238258
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-01-08 15:56:41 +0800

    Sun Jan  8 03:56:41 PM CST 2023

--- modules/contentModifiers/cPulipuliBlog.js
+++ modules/contentModifiers/cPulipuliBlog.js
@@ -46,13 +46,13 @@ const cPulipuliBlog = function (content, code, $) {
     let html = $.html()
     html = html.slice(0, html.indexOf('<title '))
     let parts = html.split(`<category scheme="http://www.blogger.com/atom/ns#" term="`)
-    // for (let i = 1; i < parts.length; i++) {
-    //   let term = part[i].slice(0, part[i].indexOf('"'))
-    //   if (term.indexOf('/') > -1) {
-    //     term = term.slice(term.lastIndexOf('/') + 1).trim()
-    //   } 
-    //   categories.push(term)
-    // }
+    for (let i = 1; i < parts.length; i++) {
+      let term = parts[i].slice(0, parts[i].indexOf('"'))
+      if (term.indexOf('/') > -1) {
+        term = term.slice(term.lastIndexOf('/') + 1).trim()
+      } 
+      categories.push(term)
+    }
   }
   catch (e) {
     // categories = e.toString()
