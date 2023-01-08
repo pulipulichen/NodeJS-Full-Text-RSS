0abbaedd568fd9ed7eae3feff52b698d3756b441
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-01-08 15:28:13 +0800

    Sun Jan  8 03:28:13 PM CST 2023

--- modules/contentModifiers/cPulipuliBlog.js
+++ modules/contentModifiers/cPulipuliBlog.js
@@ -2,6 +2,11 @@
 const cheerio = require('cheerio')
 
 const cPulipuliBlog = function (content, code, $) {
+
+  let url = $('link[rel="alternate"][type="text/html"][href]:first').attr('href')
+
+  // -------------
+
   let container = cheerio.load(content);
   let text = []
   let pList = container('p')
@@ -12,17 +17,37 @@ const cPulipuliBlog = function (content, code, $) {
     text.push(t)
     
     if (text.join('').length > 50) {
-      text.push('繼續閱讀 ⇨')
+      text.push('繼續閱讀 ⇨' + url)
       break
     }
   }
   
+  // -------------------
+
+  let categories = $('category[scheme="http://www.blogger.com/atom/ns#"][term]')
+  let terms = []
+  for (let i = 0; i < categories.length; i) {
+    let term = categories[i].attr('term')
+    if (term.indexOf('/') > -1) {
+      term = term.slice(term.lastIndexOf('/') + 1).trim()
+    }
+    terms.push(term)
+  }
+
+  terms = terms.filter((v, i, a) => a.indexOf(v) === i)
+
+  if (terms.length > 0) {
+    text.push('#' + terms.join(' #'))
+  }
+
+  // -------------------
+
   text = text.filter(t => t.trim() !== '')
 
   text = text.join('\n<br />\n<br />')
   //console.log(text)
 
-  text = '<textarea>' + $.html() + '</textarea>' + text
+  // text = '<textarea>' + $.html() + '</textarea>' + text
   
   return text
 }
