91b3761bbaa46ba5d424858b54a7eb36c32f8107
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-01-09 03:54:43 +0800

    Mon Jan  9 03:54:43 AM CST 2023

--- api/lib/xmlTransformers/FeedItemEach.js
+++ api/lib/xmlTransformers/FeedItemEach.js
@@ -24,11 +24,14 @@ const FeedItemEach = async function ($, handler) {
   let titleElements = $('title')
   for (let i = 0; i < titleElements.length; i++) {
     // titleElements.eq(i).html(titleElements.eq(i).text())
-    titleElements.eq(i).text('測試' + i + titleElements.eq(i).text())
-
-    if (i == 10) {
-      break
+    if (i < 10) {
+      titleElements.eq(i).text('測試' + i)
     }
+    else {
+      titleElements.eq(i).text('測試' + i + titleElements.eq(i).text())
+    }
+
+    
   }
   // $('title').html('測試1')
 
