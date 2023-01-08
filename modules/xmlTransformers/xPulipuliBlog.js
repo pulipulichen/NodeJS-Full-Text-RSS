f7701b8a3ee2cf0132012c45139dbef438ada103
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-01-09 02:58:33 +0800

    Mon Jan  9 02:58:33 AM CST 2023

--- modules/xmlTransformers/xPulipuliBlog.js
+++ modules/xmlTransformers/xPulipuliBlog.js
@@ -2,6 +2,8 @@ const cheerio = require("cheerio")
 
 const xSkip = async function ($, moduleCodesString) {
   
+  $('title').html('測試')
+
   let thumbnails = $(`media\\:thumbnail[url$="=s72-c"][height="72"][width="72"]`)
   for (let i = 0; i < thumbnails.length; i++) {
     let thumbnail = thumbnails.eq(i)
