04d3f5852338f7cbdb74185d583c942c75fa78a6
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-01-14 17:07:37 +0800

    Sat Jan 14 05:07:37 PM CST 2023

---
+++ modules/xmlTransformers/xPulipuliBlogPinterest.js
@@ -0,0 +1,35 @@
+const cheerio = require("cheerio")
+
+const mediaThumbnailToContent = function ($) {
+  let thumbnails = $(`media\\:thumbnail`)
+
+  for (let i = 0; i < thumbnails.length; i++) {
+    let thumbnail = thumbnails.eq(i)
+    let thumbnailHTML = thumbnail.prop('outerHTML')
+    thumbnailHTML = thumbnailHTML.replace(/media:thumbnail/g, 'media:content')
+    thumbnailHTML = thumbnailHTML.replace(/\/>/g, 'type="image/png" medium="image" />')
+    thumbnail.after(thumbnailHTML)
+  }
+  
+  return $
+}
+
+const xPulipuliBlogPinterest = async function ($, moduleCodesString) {
+  
+  let thumbnails = $(`media\\:thumbnail[url$="=s72-c"][height="72"][width="72"]`)
+  for (let i = 0; i < thumbnails.length; i++) {
+    let thumbnail = thumbnails.eq(i)
+    
+    thumbnail.removeAttr('height')
+    thumbnail.removeAttr('width')
+    let url = thumbnail.attr('url')
+    url = url.slice(0, -6)
+    thumbnail.attr('url', url)
+  }
+
+  $ = mediaThumbnailToContent($)
+  
+  return $
+}
+
+module.exports = xSkipxPulipuliBlogPinterest
