4ac4e13499a99c978135b76cdfc2d57384fbd892
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-01-19 02:44:22 +0800

    Thu Jan 19 02:44:22 AM CST 2023

--- public/components/full-text-rss-tester/full-text-rss-tester.js
+++ public/components/full-text-rss-tester/full-text-rss-tester.js
@@ -583,7 +583,7 @@ module.exports = {
     },
     copyTextToClipboard (text) {
       if (!navigator.clipboard) {
-        fallbackCopyTextToClipboard(text);
+        this.fallbackCopyTextToClipboard(text);
         return;
       }
       navigator.clipboard.writeText(text).then(function() {
@@ -591,6 +591,29 @@ module.exports = {
       }, function(err) {
         console.error('Async: Could not copy text: ', err);
       });
+    },
+    fallbackCopyTextToClipboard(text) {
+      var textArea = document.createElement("textarea");
+      textArea.value = text;
+  
+      // Avoid scrolling to bottom
+      textArea.style.top = "0";
+      textArea.style.left = "0";
+      textArea.style.position = "fixed";
+  
+      document.body.appendChild(textArea);
+      textArea.focus();
+      textArea.select();
+  
+      try {
+        var successful = document.execCommand('copy');
+        var msg = successful ? 'successful' : 'unsuccessful';
+        console.log('Fallback: Copying text command was ' + msg);
+      } catch (err) {
+        console.error('Fallback: Oops, unable to copy', err);
+      }
+  
+      document.body.removeChild(textArea);
     },
     copySubURL () {
       this.copyTextToClipboard(this.subURL)
