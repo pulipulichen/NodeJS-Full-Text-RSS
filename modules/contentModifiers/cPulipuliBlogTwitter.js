1fd1ea4145cb41eac55263827ffe5083ea803899
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-01-19 02:54:07 +0800

    Thu Jan 19 02:54:07 AM CST 2023

--- modules/contentModifiers/cPulipuliBlogTwitter.js
+++ modules/contentModifiers/cPulipuliBlogTwitter.js
@@ -5,6 +5,7 @@ const cheerio = require('cheerio')
 const textLimit = 130
 
 const isURL = function (str) {
+  str = decodeHTMLEntities(str)
   var pattern = new RegExp('^(https?:\\/\\/)?' + // protocol
           '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
           '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
@@ -12,6 +13,26 @@ const isURL = function (str) {
           '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
           '(\\#[-a-z\\d_]*)?$', 'i'); // fragment locator
   return !!pattern.test(str);
+}
+
+function decodeHTMLEntities(text) {
+  var entities = [
+      ['amp', '&'],
+      ['apos', '\''],
+      ['#x27', '\''],
+      ['#x2F', '/'],
+      ['#39', '\''],
+      ['#47', '/'],
+      ['lt', '<'],
+      ['gt', '>'],
+      ['nbsp', ' '],
+      ['quot', '"']
+  ];
+
+  for (var i = 0, max = entities.length; i < max; ++i) 
+      text = text.replace(new RegExp('&'+entities[i][0]+';', 'g'), entities[i][1]);
+
+  return text.trim()
 }
 
 const cPulipuliBlogTwitter = function (content, code, $) {
