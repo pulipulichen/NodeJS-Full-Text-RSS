966d378eea652297e00539e6b2a5f6e8279f6ac1
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-01-19 02:47:29 +0800

    Thu Jan 19 02:47:29 AM CST 2023

---
+++ modules/xmlTransformers/xPulipuliBlogTwitter.js
@@ -0,0 +1,51 @@
+const cheerio = require("cheerio")
+
+const mediaThumbnailToContent = function ($) {
+  let thumbnails = $(`media\\:thumbnail`)
+
+  for (let i = 0; i < thumbnails.length; i++) {
+    let thumbnail = thumbnails.eq(i)
+    let thumbnailHTML = thumbnail.prop('outerHTML')
+    thumbnailHTML = thumbnailHTML.replace(/media:thumbnail/g, 'media:content')
+    thumbnailHTML = thumbnailHTML.replace(/\/>/g, 'type="image/png" medium="image" />')
+    thumbnail.after(thumbnailHTML)
+
+    let enclosureHTML = thumbnailHTML
+    enclosureHTML = enclosureHTML.replace(/media:content/g, 'enclosure')
+    thumbnail.after(enclosureHTML)
+
+    // let imageHTML = thumbnailHTML
+    // let thumbnailContainer = cheerio.load(thumbnailHTML);
+    let url = thumbnail.attr('url')
+    let title = thumbnail.parent().find('title:first').text()
+    let link = thumbnail.parent().find('link:first').text()
+
+    thumbnail.after(`<image>
+    <url>${url}</url>
+    <title>${title}</title>
+    <link>${link}</link>
+  </image>`)
+  }
+  
+  return $
+}
+
+const xPulipuliBlogTwitter = async function ($, moduleCodesString) {
+  
+  let thumbnails = $(`media\\:thumbnail[url$="=s72-c"][height="72"][width="72"]`)
+  for (let i = 0; i < thumbnails.length; i++) {
+    let thumbnail = thumbnails.eq(i)
+    
+    thumbnail.removeAttr('height')
+    thumbnail.removeAttr('width')
+    let url = thumbnail.attr('url')
+    url = url.slice(0, -6)
+    thumbnail.attr('url', url)
+  }
+
+  $ = mediaThumbnailToContent($)
+  
+  return $
+}
+
+module.exports = xPulipuliBlogTwitter
